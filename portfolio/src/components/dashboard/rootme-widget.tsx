f96d1baecfb046fd65dd090327884294ee063c46
nathan.sanchez <nathan.sanchez@epita.fr>
2026-04-03 20:50:54 +0200

    t

--- portfolio/src/components/dashboard/rootme-widget.tsx
+++ portfolio/src/components/dashboard/rootme-widget.tsx
@@ -1,12 +1,11 @@
-
-
 // src/components/dashboard/rootme-widget.tsx
 import { getRootMeLeaderboard } from "@/src/lib/rootme"
 import { getFcscLeaderboard } from "@/src/lib/fcsc"
 import LeaderboardClient from "./leaderboard"
 
 export default async function RootMeWidget() {
-  const pbUrl = process.env.NEXT_PUBLIC_PB_URL || 'http://127.0.0.1:8090';
+  // 🛠️ LE CORRECTIF : On cherche l'URL interne en priorité (pour Docker), sinon l'URL externe, sinon localhost
+  const pbUrl = process.env.PB_INTERNAL_URL || process.env.NEXT_PUBLIC_PB_URL || 'http://127.0.0.1:8090';
   let rootmePlayers = [];
   let fcscPlayers = [];
 
@@ -16,22 +15,24 @@ export default async function RootMeWidget() {
       const data = await res.json();
       const allPlayers = data.items || [];
       
-     rootmePlayers = allPlayers
+      rootmePlayers = allPlayers
         .filter((p: any) => p.platform === 'rootme')
         .map((p: any) => ({ 
           pseudo: p.pseudo, 
           login: p.account_id,
-          // BLINDAGE : On s'assure que c'est bien un nombre valide et non vide
           id: (p.numeric_id && p.numeric_id !== 0) ? Number(p.numeric_id) : undefined 
         }));
-
-      console.log(rootmePlayers)
         
       fcscPlayers = allPlayers
         .filter((p: any) => p.platform === 'fcsc')
         .map((p: any) => parseInt(p.account_id));
+    } else {
+      console.error("[Dashboard] PocketBase a refusé la connexion HTTP:", res.status);
     }
-  } catch(e) { console.error("Erreur chargement joueurs PB"); }
+  } catch(e) { 
+    // Si Hairpin NAT bloque la requête, on le verra clairement dans 'docker logs' !
+    console.error("[Dashboard] Erreur réseau critique vers PocketBase :", e); 
+  }
 
   const [rootmeData, fcscData] = await Promise.all([
     getRootMeLeaderboard(rootmePlayers),
